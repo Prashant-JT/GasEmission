d2b405fa7cad70b227f46ff57a979e9e89c8718d
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2021-01-08 12:31:52 +0000

    Dataset

---
+++ script.m
@@ -0,0 +1,44 @@
+clc; clear;
+
+dataset = importdata("dataset/gt_2015.csv");
+features = dataset.data;
+
+meanFeatures = mean(features, 1);
+
+XC = zeros(size(features));
+for i=1:size(features, 2)
+    XC(:, i) = features(:, i) - meanFeatures(i);
+end
+
+Z = (XC'*XC)/size(features, 1);
+
+[V, D] = eig(Z);
+diagonal = diag(D)';
+
+%figure(1)
+%scatter(features(:, 2), features(:, 3));
+figure(1)
+scatter(XC(:, 1), XC(:, 3));
+
+figure(2)
+mult = features * V;
+scatter(mult(:, 1), mult(:, 3));
+
+
+%{
+scatter(V);
+
+
+
+m = 5000; n = 2;
+A = randn(m,n);
+% deformación por un factor de 3
+A(:,2) = 3*A(:,2);
+% matriz de rotación
+phi = 45;
+cose = cosd(phi); sen = sind(phi);
+R = [cose -sen; sen cose];
+% rotación y traslacion al punto (10,10)
+B = A*R + 10;
+scatter(B(:, 1), B(:, 2));
+%}
